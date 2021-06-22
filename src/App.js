fba7579b0a965cadf15a3924e3587ad2822227ba
Tomasz Dańczak <tomaszdanczak@gmail.com>
2021-06-22 19:41:32 +0200

    feat: add JSX of Switch

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
 import Sidebar from "./Components/Sidebar";
 import styled from "styled-components";
+import { Route, Switch } from "react-router";
+import HomePage from "./Pages/HomePage";
+import AboutPage from "./Pages/AboutPage";
+import ResumePage from "./Pages/ResumePage";
+import PortfoliosPage from "./Pages/PortfoliosPage";
+import BlogsPage from "./Pages/BlogsPage";
+import ContactPage from "./Pages/ContactPage";
 
 function App() {
   return (
     <div className="App">
       <Sidebar />
-      <MainContentStyled></MainContentStyled>
+
+      <MainContentStyled>
+        <Switch>
+          <Route path="/" exact>
+            <HomePage />
+          </Route>
+          <Route path="/about" exact>
+            <AboutPage />
+          </Route>
+          <Route path="/resume" exact>
+            <ResumePage />
+          </Route>
+          <Route path="/portfolios" exact>
+            <PortfoliosPage />
+          </Route>
+          <Route path="/blogs" exact>
+            <BlogsPage />
+          </Route>
+          <Route path="/contact" exact>
+            <ContactPage />
+          </Route>
+        </Switch>
+      </MainContentStyled>
     </div>
   );
 }
