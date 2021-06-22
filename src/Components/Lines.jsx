22e604c9fbcb700eb0cffcb07085a598217430dc
Tomasz Dańczak <tomaszdanczak@gmail.com>
2021-06-22 19:49:19 +0200

    feat: add styles to the Lines component

--- src/Components/Lines.jsx
+++ src/Components/Lines.jsx
@@ -12,6 +12,22 @@ function Lines() {
   )
 }
 
-const LinesStyled = styled.div``
+const LinesStyled = styled.div`
+  position: absolute;
+  min-height: 100%;
+  width: 100%;
+  display: flex;
+  justify-content: space-evenly;
+  opacity: 0.4;
+  z-index: -1;
+
+  .line-1,
+  .line-2,
+  .line-3,
+  .line-4 {
+    width: 1px;
+    min-height: 100vh;
+    background-color: var(--border-color);
+  }`
 
 export default Lines
