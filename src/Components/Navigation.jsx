318e4654a91b1c64b01ee2b00eabb5ec5eb3f335
Tomasz Dańczak <tomaszdanczak@gmail.com>
2021-06-22 19:00:02 +0200

    feat: create whole JSX in the Navigation component

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -9,13 +9,43 @@ function Navigation() {
       <div className="avatar">
         <img src={avatar} alt="" />
       </div>
+
       <ul className="nav-items">
         <li className="nav-item">
-          <NavLink to="/home" activeClassName="active-class">
+          <NavLink to="/" activeClassName="active-class" exact>
             Home
           </NavLink>
         </li>
+        <li className="nav-item">
+          <NavLink to="/about" activeClassName="active-class" exact>
+            About
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink to="/resume" activeClassName="active-class" exact>
+            Resume
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink to="/portfolios" activeClassName="active-class" exact>
+            Portfolios
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink to="/blogs" activeClassName="active-class" exact>
+            Blogs
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink to="/contact" activeClassName="active-class" exact>
+            Contact
+          </NavLink>
+        </li>
       </ul>
+
+      <footer className="footer">
+        <p>@2021 My Portfolio Website</p>
+      </footer>
     </NavigationStyled>
   );
 }
