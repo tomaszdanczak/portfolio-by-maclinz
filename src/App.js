ef50bc4cbfa270189746c2dfdb508b1d170760eb
Tomasz Dańczak <tomaszdanczak@gmail.com>
2021-06-22 19:27:46 +0200

    feat: add simple MainContent

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import Sidebar from "./Components/Sidebar";
+import styled from "styled-components";
 
 function App() {
   return (
     <div className="App">
       <Sidebar />
+      <MainContentStyled></MainContentStyled>
     </div>
   );
 }
+
+const MainContentStyled = styled.main``
 
 export default App;
