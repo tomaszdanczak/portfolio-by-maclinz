7ec3825d9734013e6e86ac3db8680f64728e46c6
Tomasz Dańczak <tomaszdanczak@gmail.com>
2021-06-22 19:44:31 +0200

    feat: add simple Lines component and render it

---
+++ src/Components/Lines.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Lines() {
+  return (
+    <LinesStyled></LinesStyled>
+  )
+}
+
+const LinesStyled = styled.div``
+
+export default Lines
