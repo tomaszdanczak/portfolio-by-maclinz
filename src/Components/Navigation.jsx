04e921b7811221831daef15fd0da51de9b4774aa
Tomasz Dańczak <tomaszdanczak@gmail.com>
2021-06-22 19:09:11 +0200

    feat: add styles to the navlist

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -70,6 +70,48 @@ const NavigationStyled = styled.nav`
       border: 8px solid var(--border-color);
     }
   }
+
+  .nav-items {
+    width: 100%;
+    text-align: center;
+    .active-class {
+      background-color: var(--primary-color);
+    }
+    li {
+      display: block;
+      a {
+        display: block;
+        padding: 0.45rem 0;
+        position: relative;
+        z-index: 10;
+        text-transform: uppercase;
+        transition: all 0.4s ease-in-out;
+        font-weight: 600;
+        letter-spacing: 1px;
+        &:hover {
+          cursor: pointer;
+          color: var(--white-color);
+        }
+        &::before {
+          content: "";
+          position: absolute;
+          bottom: 0;
+          left: 0;
+          width: 0;
+          height: 50%;
+          background-color: var(--primary-color);
+          transition: all 0.4s cubic-bezier(0, -0.2, 0.25, 0.95);
+          z-index: 3;
+          opacity: 0.2;
+          z-index: -1;
+        }
+      }
+      a:hover::before {
+        width: 100%;
+        height: 100%;
+      }
+    }
+  }
   `;
 
 export default Navigation;
--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -25,10 +25,19 @@ const GlobalStyle = createGlobalStyle`
   box-sizing: border-box;
   list-style: none;
   text-decoration: none;
+  font-family: "Nunito", sans-serif;
+  font-size: 1.1rem;
 }
+
 body {
   background-color: var(--background-dark-color);
   color: var(--font-light-color);
+}
+
+a {
+  font-family: inherit;
+  color: inherit;
+  font-size: 1rem;
 }
 `;
 
