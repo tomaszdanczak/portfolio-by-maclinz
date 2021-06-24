270d6788f5f309ad22b3f1b16fd4851083c4039f
Tomasz Dańczak <tomaszdanczak@gmail.com>
2021-06-24 18:36:15 +0200

    feat: add styles to the typography container

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -39,6 +39,15 @@ const HomePageStyled = styled.header`
   width: 100%;
   height: 100vh;
   position: relative;
+
+  .typography {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    text-align: center;
+    width: 80%;
+  }
 `;
 
 export default HomePage;
