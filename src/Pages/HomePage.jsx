e923851c62887e632aa66f3ea6728a9e4839267a
Tomasz Dańczak <tomaszdanczak@gmail.com>
2021-06-24 18:30:39 +0200

    feat: create JSX of HomePage component

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,12 +1,36 @@
 import React from "react";
 import styled from "styled-components";
 import Particle from "../Components/Particle";
+import FacebookIcon from "@material-ui/icons/Facebook";
+import GithubIcon from "@material-ui/icons/GitHub";
+import YoutubeIcon from "@material-ui/icons/YouTube";
 
 function HomePage() {
   return (
     <HomePageStyled>
       <Particle />
-      Home Page
+
+      <div className="typography">
+        <h1>
+          Hi, I'm <span>Lorem Ipsum</span>
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
+          odit voluptate dolores eligendi quia, dolorem accusantium corporis quo
+          dignissimos nisi! Lorem ipsum dolor sit amet.
+        </p>
+        <div className="icons">
+          <a href="https://codepen.io/pen" className="icon i-facebook">
+            <FacebookIcon />
+          </a>
+          <a href="https://codepen.io/pen" className="icon i-github">
+            <GithubIcon />
+          </a>
+          <a href="https://codepen.io/pen" className="icon i-youtube">
+            <YoutubeIcon />
+          </a>
+        </div>
+      </div>
     </HomePageStyled>
   );
 }
