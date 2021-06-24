1aecaeefbd17fc1775aca3c67c04478443ddb164
Tomasz Dańczak <tomaszdanczak@gmail.com>
2021-06-24 18:00:54 +0200

    feat: add styled component

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,7 +1,10 @@
 import React from "react";
+import styled from "styled-components";
 
 function HomePage() {
-  return <div>Home Page</div>;
+  return <HomePageStyled>Home Page</HomePageStyled>;
 }
+
+const HomePageStyled = styled.header``;
 
 export default HomePage;
