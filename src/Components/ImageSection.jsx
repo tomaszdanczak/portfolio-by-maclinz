ec42d008510b956b405d4aa9eddfa646600b6697
Tomasz Dańczak <tomaszdanczak@gmail.com>
2021-06-25 19:40:44 +0200

    feat: create simple ImageSection component and render it in About Page

---
+++ src/Components/ImageSection.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+function ImageSection() {
+  return <ImageSectionStyled>Image Section</ImageSectionStyled>;
+}
+
+const ImageSectionStyled = styled.div``;
+
+export default ImageSection;
--- src/Pages/AboutPage.jsx
+++ src/Pages/AboutPage.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import styled from "styled-components";
 import { MainLayout } from "../styles/Layouts";
 import Title from "../Components/Title";
+import ImageSection from "../Components/ImageSection";
 
 function AboutPage() {
   return (
     <MainLayout>
       <AboutStyled>
         <Title title={"About Me"} span={"About Me"} />
+        <ImageSection />
       </AboutStyled>
     </MainLayout>
   );
