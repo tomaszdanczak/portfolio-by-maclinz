1b5da0ae213b9d442e61e2c572bd636e9eb5d7ee
Tomasz Dańczak <tomaszdanczak@gmail.com>
2021-06-26 07:53:25 +0200

    feat: create simple ServiceCard component and render it in ServicesSection

---
+++ src/Components/ServiceCard.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+function ServiceCard() {
+  return <ServiceCardStyled>Service Card</ServiceCardStyled>;
+}
+
+const ServiceCardStyled = styled.div``;
+
+export default ServiceCard;
--- src/Components/ServicesSection.jsx
+++ src/Components/ServicesSection.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import styled from "styled-components";
 import { InnerLayout } from "../styles/Layouts";
 import Title from "./Title";
+import ServiceCard from "./ServiceCard";
 
 function ServicesSection() {
   return (
     <InnerLayout>
       <ServicesSectionStyled>
         <Title title={"services"} span={"services"} />
+        <div className="services">
+          <ServiceCard />
+        </div>
       </ServicesSectionStyled>
     </InnerLayout>
   );
