23be773c65dc1e72d51944f4cace1d35a9f42e97
Tomasz Dańczak <tomaszdanczak@gmail.com>
2021-06-26 08:57:22 +0200

    feat: add Title component in ReviewsSection

--- src/Components/ReviewsSection.jsx
+++ src/Components/ReviewsSection.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import styled from "styled-components";
 import { InnerLayout } from "../styles/Layouts";
+import Title from "./Title";
 
 function ReviewsSection() {
   return (
     <InnerLayout>
-      <ReviewsSectionStyled>Reviews Section</ReviewsSectionStyled>
+      <ReviewsSectionStyled>
+        <Title title={"reviews"} span={"reviews"} />
+      </ReviewsSectionStyled>
     </InnerLayout>
   );
 }
