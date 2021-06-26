badb8d5abab494264ba7c5023d07b4772cc32d03
Tomasz Dańczak <tomaszdanczak@gmail.com>
2021-06-26 09:17:05 +0200

    feat: add styles to the ReviewItem component

--- src/Components/ReviewItem.jsx
+++ src/Components/ReviewItem.jsx
@@ -9,6 +9,26 @@ function ReviewItem({ text }) {
   );
 }
 
-const ReviewItemStyled = styled.div``;
+const ReviewItemStyled = styled.div`
+  padding: 2rem 1rem;
+  border-left: 6px solid var(--border-color);
+  background-color: var(--background-dark-grey);
+  position: relative;
+
+  &::after {
+    content: "";
+    position: absolute;
+    left: 2rem;
+    border-width: 0.8rem;
+    top: 100%;
+    border-style: solid;
+    border-color: var(--background-dark-grey) transparent transparent
+      var(--background-dark-grey);
+  }
+
+  p {
+    padding: 1rem 0;
+  }
+`;
 
 export default ReviewItem;
