1962b6470d49f86bddd74306a65c755040c25f55
Tomasz Dańczak <tomaszdanczak@gmail.com>
2021-06-27 10:34:57 +0200

    feat: create JSX in Blogs Page using data from blogs.js

--- src/Pages/BlogsPage.jsx
+++ src/Pages/BlogsPage.jsx
@@ -2,13 +2,27 @@ import React from "react";
 import styled from "styled-components";
 import { InnerLayout, MainLayout } from "../styles/Layouts";
 import Title from "../Components/Title";
+import blogs from "../data/blogs";
 
 function BlogsPage() {
   return (
     <MainLayout>
       <BlogsPageStyled>
         <Title title={"Blogs"} span={"Blogs"} />
-        <InnerLayout className={"blog"}>Blogs Page</InnerLayout>
+        <InnerLayout className={"blog"}>
+          {blogs.map((blog) => {
+            return (
+              <div key={blog.id} className="blog-item">
+                <div className="image">
+                  <img src={blog.image} alt="" />
+                </div>
+                <div className="title">
+                  <a href={blog.link}>{blog.title}</a>
+                </div>
+              </div>
+            );
+          })}
+        </InnerLayout>
       </BlogsPageStyled>
     </MainLayout>
   );
