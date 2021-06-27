d610bb51a245ee47fbb5bb1712fb84cb6221ed7d
Tomasz Dańczak <tomaszdanczak@gmail.com>
2021-06-27 10:20:59 +0200

    feat: add Title component

--- src/Pages/BlogsPage.jsx
+++ src/Pages/BlogsPage.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import styled from "styled-components";
 import { InnerLayout, MainLayout } from "../styles/Layouts";
+import Title from "../Components/Title";
 
 function BlogsPage() {
   return (
     <MainLayout>
       <BlogsPageStyled>
+        <Title title={"Blogs"} span={"Blogs"} />
         <InnerLayout className={"blog"}>Blogs Page</InnerLayout>
       </BlogsPageStyled>
     </MainLayout>
