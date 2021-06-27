e8d143cb066896086ad0b3051678a37a6de90e7b
Tomasz Dańczak <tomaszdanczak@gmail.com>
2021-06-27 10:18:44 +0200

    feat: add InnerLayout, MainLayout and styled component to the Blogs Page

--- src/Pages/BlogsPage.jsx
+++ src/Pages/BlogsPage.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import styled from "styled-components";
+import { InnerLayout, MainLayout } from "../styles/Layouts";
 
 function BlogsPage() {
-  return <div>Blogs Page</div>;
+  return (
+    <MainLayout>
+      <BlogsPageStyled>
+        <InnerLayout className={"blog"}>Blogs Page</InnerLayout>
+      </BlogsPageStyled>
+    </MainLayout>
+  );
 }
+
+const BlogsPageStyled = styled.div``;
 
 export default BlogsPage;
