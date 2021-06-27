b50b0167f7d83af9bca8d9bba4f285588514f8e1
Tomasz Dańczak <tomaszdanczak@gmail.com>
2021-06-27 17:39:21 +0200

    feat: add styles to the Blogs Page

--- src/Pages/BlogsPage.jsx
+++ src/Pages/BlogsPage.jsx
@@ -28,6 +28,49 @@ function BlogsPage() {
   );
 }
 
-const BlogsPageStyled = styled.div``;
+const BlogsPageStyled = styled.div`
+  .blog {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-column-gap: 2rem;
+    grid-row-gap: 3rem;
+  }
+
+  .blog-item {
+    background-color: var(--background-dark-grey);
+    padding: 1rem 1rem;
+  }
+
+  .image {
+    width: 100%;
+    overflow: hidden;
+    padding-bottom: 0.5rem;
+
+    img {
+      width: 100%;
+      height: 90%;
+      object-fit: cover;
+      transition: all 0.4s ease-in-out;
+
+      &:hover {
+        cursor: pointer;
+        transform: rotate(3deg) scale(1.1);
+      }
+    }
+  }
+
+  .title {
+    a {
+      font-size: 1.8rem;
+      color: var(--white-color);
+      cursor: pointer;
+      transition: all 0.4s ease-in-out;
+
+      &:hover {
+        color: var(--primary-color);
+      }
+    }
+  }
+`;
 
 export default BlogsPage;
