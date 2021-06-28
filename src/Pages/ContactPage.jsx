a81f5772adba2339918a0540f84884b650380ace
Tomasz Dańczak <tomaszdanczak@gmail.com>
2021-06-28 18:16:08 +0200

    feat: add styles to the contact-section and left-content

--- src/Pages/ContactPage.jsx
+++ src/Pages/ContactPage.jsx
@@ -54,6 +54,59 @@ function ContactPage() {
   );
 }
 
-const ContactPageStyled = styled.section``;
+const ContactPageStyled = styled.section`
+  .contact-section {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-column-gap: 2rem;
+  }
+
+  .contact-title {
+    h4 {
+      color: var(--white-color);
+      padding: 1rem 0;
+      font-size: 1.8rem;
+    }
+  }
+
+  .form {
+    width: 100%;
+
+    .form-field {
+      margin-top: 2rem;
+      position: relative;
+      width: 100%;
+
+      label {
+        position: absolute;
+        left: 20px;
+        top: -19px;
+        display: inline-block;
+        background-color: var(--background-dark-color);
+        padding: 0 0.5rem;
+        color: inherit;
+      }
+
+      input {
+        border: 1px solid var(--border-color);
+        outline: none;
+        background: transparent;
+        height: 50px;
+        padding: 0 15px;
+        width: 100%;
+        color: inherit;
+      }
+
+      textarea {
+        background-color: transparent;
+        border: 1px solid var(--border-color);
+        outline: none;
+        color: inherit;
+        width: 100%;
+        padding: 0.8rem 1rem;
+      }
+    }
+  }
+`;
 
 export default ContactPage;
