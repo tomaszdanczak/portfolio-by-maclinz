37b72f244ff7a8827376b9a43ba989543b1b7086
Tomasz Dańczak <tomaszdanczak@gmail.com>
2021-06-28 19:49:09 +0200

    feat: add styles to the ProgressBar

--- src/Components/ProgressBar.jsx
+++ src/Components/ProgressBar.jsx
@@ -15,6 +15,34 @@ function ProgressBar({ title, width, text }) {
   );
 }
 
-const ProgressBarStyled = styled.div``;
+const ProgressBarStyled = styled.div`
+  h6 {
+    text-transform: uppercase;
+  }
+
+  .progress-bar {
+    display: flex;
+    align-items: center;
+
+    p {
+      padding-right: 1.1rem;
+    }
+
+    .progress {
+      position: relative;
+      width: 100%;
+      height: 0.4rem;
+      background-color: var(--border-color);
+
+      span {
+        position: absolute;
+        left: 0;
+        bottom: 0;
+        height: 100%;
+        background-color: var(--primary-color);
+      }
+    }
+  }
+`;
 
 export default ProgressBar;
