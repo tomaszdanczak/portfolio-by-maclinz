236393a62144ba1d9878fd3cc0092d5f0eac10a1
Tomasz Dańczak <tomaszdanczak@gmail.com>
2021-06-28 18:20:49 +0200

    feat: create simple ContactItem component

---
+++ src/Components/ContactItem.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+function ContactItem({ icon, title, contact1, contact2 }) {
+  return <ContactItemStyled>Contact Item</ContactItemStyled>;
+}
+
+const ContactItemStyled = styled.div``;
+
+export default ContactItem;
