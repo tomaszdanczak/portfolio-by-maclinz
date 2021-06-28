307632c1bf60f6fd54bed1d94df1c8f1615bd811
Tomasz Dańczak <tomaszdanczak@gmail.com>
2021-06-28 19:13:48 +0200

    feat: add styled component and InnerLayout in Skills component

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import styled from "styled-components";
+import { InnerLayout } from "../styles/Layouts";
 
 function Skills() {
-  return <section>Skills</section>;
+  return (
+    <SkillsStyled>
+      <InnerLayout>Skills</InnerLayout>
+    </SkillsStyled>
+  );
 }
+
+const SkillsStyled = styled.section``;
 
 export default Skills;
