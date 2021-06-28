7077385f6d6215732baaccda1ba829ff22b32696
Tomasz Dańczak <tomaszdanczak@gmail.com>
2021-06-28 19:19:57 +0200

    feat: add Title component

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import styled from "styled-components";
 import { InnerLayout } from "../styles/Layouts";
+import Title from "./Title";
 
 function Skills() {
   return (
     <SkillsStyled>
+      <Title title={"My Skills"} span={"My Skills"} />
+
       <InnerLayout>Skills</InnerLayout>
     </SkillsStyled>
   );
