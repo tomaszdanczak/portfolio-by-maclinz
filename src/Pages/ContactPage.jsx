b75b31f3c461d0ac93ffaf8cd6ae0b97c62690d5
Tomasz Dańczak <tomaszdanczak@gmail.com>
2021-06-28 17:33:35 +0200

    feat: add Title component

--- src/Pages/ContactPage.jsx
+++ src/Pages/ContactPage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import styled from "styled-components";
 import { MainLayout, InnerLayout } from "../styles/Layouts";
+import Title from "../Components/Title";
 
 function ContactPage() {
   return (
     <MainLayout>
+      <Title title={"Contact"} span={"Contact"} />
       <ContactPageStyled>
         <InnerLayout className={"contact-section"}>Contact Page</InnerLayout>
       </ContactPageStyled>
