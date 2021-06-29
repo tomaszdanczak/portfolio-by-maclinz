979439ff845583c5cb4d1242109b81cb7502b353
Tomasz Dańczak <tomaszdanczak@gmail.com>
2021-06-29 17:17:25 +0200

    feat: add styled component and InnerLayout in Resume component

--- src/Components/Resume.jsx
+++ src/Components/Resume.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import styled from "styled-components";
+import { InnerLayout } from "../styles/Layouts";
 
 function Resume() {
-  return <div>Resume</div>;
+  return (
+    <ResumeStyled>
+      <InnerLayout>Resume</InnerLayout>
+    </ResumeStyled>
+  );
 }
+
+const ResumeStyled = styled.section``;
 
 export default Resume;
