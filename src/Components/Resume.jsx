c88112e62f27abc7fd80d835a83c93024f9421b9
Tomasz Dańczak <tomaszdanczak@gmail.com>
2021-06-29 17:57:31 +0200

    feat: render ResumeItem in Resume

--- src/Components/Resume.jsx
+++ src/Components/Resume.jsx
@@ -4,6 +4,7 @@ import { InnerLayout } from "../styles/Layouts";
 import Title from "./Title";
 import SmallTitle from "./SmallTitle";
 import BusinessCenterIcon from "@material-ui/icons/BusinessCenter";
+import ResumeItem from "./ResumeItem";
 
 function Resume() {
   const briefcase = <BusinessCenterIcon />;
@@ -15,6 +16,35 @@ function Resume() {
       <InnerLayout>
         <div className="small-title">
           <SmallTitle icon={briefcase} title={"Working Experience"} />
+        </div>
+
+        <div className="resume-content">
+          <ResumeItem
+            year={"2015 - 2020"}
+            title={"Computer Science Teacher"}
+            subTitle={"Sussex University"}
+            text={
+              "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta, culpa doloremque nesciunt porro ullam id maiores aut ipsum! Recusandae, illo."
+            }
+          />
+
+          <ResumeItem
+            year={"2015 - Present"}
+            title={"Full Stack Developer"}
+            subTitle={"Microsoft Studios"}
+            text={
+              "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta, culpa doloremque nesciunt porro ullam id maiores aut ipsum! Recusandae, illo."
+            }
+          />
+
+          <ResumeItem
+            year={"2010 - 2017"}
+            title={"User Interface Designer"}
+            subTitle={"Google Inc"}
+            text={
+              "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta, culpa doloremque nesciunt porro ullam id maiores aut ipsum! Recusandae, illo."
+            }
+          />
         </div>
       </InnerLayout>
     </ResumeStyled>
