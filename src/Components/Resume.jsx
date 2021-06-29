45f417aaba1a21296769b4a58b81027ea8d15e3f
Tomasz Dańczak <tomaszdanczak@gmail.com>
2021-06-29 17:29:14 +0200

    feat: add Title component

--- src/Components/Resume.jsx
+++ src/Components/Resume.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import styled from "styled-components";
 import { InnerLayout } from "../styles/Layouts";
+import Title from "./Title";
 
 function Resume() {
   return (
     <ResumeStyled>
+      <Title title={"Resume"} span={"Resume"} />
       <InnerLayout>Resume</InnerLayout>
     </ResumeStyled>
   );
