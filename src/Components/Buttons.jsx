5187f3446a6afa2e9095ea497645ebbf8bb6ebae
Tomasz Dańczak <tomaszdanczak@gmail.com>
2021-06-29 20:15:30 +0200

    feat: use props in Buttons component

--- src/Components/Buttons.jsx
+++ src/Components/Buttons.jsx
@@ -1,10 +1,22 @@
 import React from "react";
 import styled from "styled-components";
 
-function Buttons() {
-  return <ButtonsStyled></ButtonsStyled>;
+function Buttons({ filter, buttons }) {
+  return (
+    <ButtonsStyled>
+      {buttons.map((but, i) => {
+        return (
+          <ButtonStyled key={i} onClick={() => filter(but)}>
+            {but}
+          </ButtonStyled>
+        );
+      })}
+    </ButtonsStyled>
+  );
 }
 
 const ButtonsStyled = styled.div``;
+
+const ButtonStyled = styled.button``;
 
 export default Buttons;
