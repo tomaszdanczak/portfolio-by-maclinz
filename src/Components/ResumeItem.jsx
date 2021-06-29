02967c19fce0f9ebe9f3f6fc80cce4dfb9cf957d
Tomasz Dańczak <tomaszdanczak@gmail.com>
2021-06-29 17:54:36 +0200

    feat: create simple ResumeItem

---
+++ src/Components/ResumeItem.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+function ResumeItem() {
+  return <ResumeItemStyled>Resume Item</ResumeItemStyled>;
+}
+
+const ResumeItemStyled = styled.div``;
+
+export default ResumeItem;
