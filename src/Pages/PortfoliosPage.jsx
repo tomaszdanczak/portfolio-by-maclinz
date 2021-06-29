c69732d002da56b6bb0a46266db0e6e9638d4cee
Tomasz Dańczak <tomaszdanczak@gmail.com>
2021-06-29 18:40:48 +0200

    feat: add Title component

--- src/Pages/PortfoliosPage.jsx
+++ src/Pages/PortfoliosPage.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import styled from "styled-components";
 import { MainLayout, InnerLayout } from "../styles/Layouts";
+import Title from "../Components/Title";
 
 function PortfoliosPage() {
   return (
     <PortfoliosPageStyled>
       <MainLayout>
+        <Title title={"Portfolios"} span={"Portfolios"} />
         <InnerLayout>Portfolios Page</InnerLayout>
       </MainLayout>
     </PortfoliosPageStyled>
