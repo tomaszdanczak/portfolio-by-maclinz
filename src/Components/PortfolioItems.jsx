d7a3228c62aadd62529a86bd18a688b00366524e
Tomasz Dańczak <tomaszdanczak@gmail.com>
2021-06-29 19:17:17 +0200

    feat: create JSX of PortfolioItems component

--- src/Components/PortfolioItems.jsx
+++ src/Components/PortfolioItems.jsx
@@ -1,8 +1,38 @@
 import React from "react";
 import styled from "styled-components";
+import GitHub from "@material-ui/icons/GitHub";
+import Pinterest from "@material-ui/icons/Pinterest";
 
-function PortfolioItems() {
-  return <PortfolioItemsStyled>Portfolio Items</PortfolioItemsStyled>;
+function PortfolioItems({ portfolioItems }) {
+  return (
+    <PortfolioItemsStyled>
+      {portfolioItems.map((item) => {
+        return (
+          <div className="grid-item" key={item.id}>
+            <div className="portfolio-content">
+              <div className="portfolio-image">
+                <img src={item.image} alt="" />
+                <ul>
+                  <li>
+                    <a href={item.link1}>
+                      <GitHub />
+                    </a>
+                  </li>
+                  <li>
+                    <a href={item.link2}>
+                      <Pinterest />
+                    </a>
+                  </li>
+                </ul>
+              </div>
+              <h6>{item.title}</h6>
+              <p>{item.text}</p>
+            </div>
+          </div>
+        );
+      })}
+    </PortfolioItemsStyled>
+  );
 }
 
 const PortfolioItemsStyled = styled.div``;
