5ff8e3b188302f39ce62d3c91baa77281128d346
Tomasz Dańczak <tomaszdanczak@gmail.com>
2021-06-29 20:00:58 +0200

    feat: create simple Buttons component

---
+++ src/Components/Buttons.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+function Buttons() {
+  return <ButtonsStyled></ButtonsStyled>;
+}
+
+const ButtonsStyled = styled.div``;
+
+export default Buttons;
