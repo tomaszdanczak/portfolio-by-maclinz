b5ed2f00e6665ecd91b706abadd597c53a75f263
Tomasz Dańczak <tomaszdanczak@gmail.com>
2021-06-29 19:18:56 +0200

    feat: add styles to the PortfolioItems component

--- src/Components/PortfolioItems.jsx
+++ src/Components/PortfolioItems.jsx
@@ -35,6 +35,105 @@ function PortfolioItems({ portfolioItems }) {
   );
 }
 
-const PortfolioItemsStyled = styled.div``;
+const PortfolioItemsStyled = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 2rem;
+
+  .grid-item {
+    .portfolio-content {
+      display: block;
+      position: relative;
+      overflow: hidden;
+
+      h6 {
+        font-size: 1.5rem;
+      }
+
+      img {
+        width: 100%;
+        height: 30vh;
+        object-fit: cover;
+      }
+
+      ul {
+        transform: translateY(-600px);
+        transition: all 0.4s ease-in-out;
+        position: absolute;
+        left: 50%;
+        top: 40%;
+        opacity: 0;
+      }
+
+      .portfolio-image {
+        &::before {
+          content: "";
+          position: absolute;
+          left: 2%;
+          top: 4%;
+          height: 0;
+          width: 0;
+          transition: all 0.4s ease-in-out;
+        }
+      }
+
+      .portfolio-image:hover {
+        ul {
+          transform: translateY(0);
+          transform: translate(-50%, -50%);
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          opacity: 1;
+
+          li {
+            background-color: var(--border-color);
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            padding: 1rem;
+            border-radius: 50%;
+            width: 3rem;
+            height: 3rem;
+            transition: all 0.4s ease-in-out;
+
+            &:not(:last-child) {
+              margin-right: 1rem;
+            }
+
+            &:hover {
+              background-color: var(--primary-color);
+            }
+
+            a {
+              display: flex;
+              align-items: center;
+              justify-content: center;
+            }
+          }
+
+          li:hover {
+            svg {
+              color: var(--white-color);
+            }
+          }
+
+          svg {
+            font-size: 2rem;
+          }
+        }
+
+        &::before {
+          height: calc(100% - 32%);
+          width: calc(100% - 4%);
+          background-color: white;
+          opacity: 0.9;
+          transform-origin: left;
+          transition: all 0.4s ease-in-out;
+        }
+      }
+    }
+  }
+`;
 
 export default PortfolioItems;
