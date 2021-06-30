99134b29749c0ce1b23877812958cafdb113c0b6
Tomasz Dańczak <tomaszdanczak@gmail.com>
2021-06-30 17:33:59 +0200

    feat: add JSX of DarkModeSwitch component

--- src/Components/DarkModeSwitch.jsx
+++ src/Components/DarkModeSwitch.jsx
@@ -1,8 +1,25 @@
 import React from "react";
 import styled from "styled-components";
+import Brightness4Icon from "@material-ui/icons/Brightness4";
+import Switch from "@material-ui/core/Switch";
 
 function DarkModeSwitch() {
-  return <DarkModeSwitchStyled>DarkModeSwitch</DarkModeSwitchStyled>;
+  return (
+    <DarkModeSwitchStyled>
+      <div className="left-content">
+        <Brightness4Icon />
+      </div>
+      <div className="right-content">
+        <Switch
+          value=""
+          // checked={}
+          inputProps={{ "aria-label": "" }}
+          size="medium"
+          // onClick={}
+        />
+      </div>
+    </DarkModeSwitchStyled>
+  );
 }
 
 const DarkModeSwitchStyled = styled.div``;
