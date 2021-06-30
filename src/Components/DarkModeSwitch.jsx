c256976c40a5d1743487d0a04a554220bbe9ba22
Tomasz Dańczak <tomaszdanczak@gmail.com>
2021-06-30 17:47:18 +0200

    feat: add styles to the DarkModeSwitch component

--- src/Components/DarkModeSwitch.jsx
+++ src/Components/DarkModeSwitch.jsx
@@ -22,6 +22,24 @@ function DarkModeSwitch() {
   );
 }
 
-const DarkModeSwitchStyled = styled.div``;
+const DarkModeSwitchStyled = styled.div`
+  position: fixed;
+  right: 0;
+  top: 35%;
+  background-color: var(--background-light-color-2);
+  width: 6.5rem;
+  height: 2.5rem;
+  z-index: 15;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  svg {
+    display: flex;
+    align-items: center;
+    font-size: 1.7rem;
+    color: var(--white-color);
+  }
+`;
 
 export default DarkModeSwitch;
