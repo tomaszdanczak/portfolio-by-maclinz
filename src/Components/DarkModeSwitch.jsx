b144101fbfa677026e5e7b81e043c98bdfac759a
Tomasz Dańczak <tomaszdanczak@gmail.com>
2021-06-30 17:27:33 +0200

    feat: create simple DarkModeSwitch component and render it

---
+++ src/Components/DarkModeSwitch.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+function DarkModeSwitch() {
+  return <DarkModeSwitchStyled>DarkModeSwitch</DarkModeSwitchStyled>;
+}
+
+const DarkModeSwitchStyled = styled.div``;
+
+export default DarkModeSwitch;
