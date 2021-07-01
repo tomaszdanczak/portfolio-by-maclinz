f9e1aebb2752c1d5b3093573381abcce3221c1c2
Tomasz Dańczak <tomaszdanczak@gmail.com>
2021-07-01 17:16:51 +0200

    feat: add media queries to the ImageSection component

--- src/Components/ImageSection.jsx
+++ src/Components/ImageSection.jsx
@@ -46,6 +46,14 @@ const ImageSectionStyled = styled.div`
   margin-top: 5rem;
   display: flex;
 
+  @media screen and (max-width: 1000px) {
+    flex-direction: column;
+
+    .left-content {
+      margin-bottom: 2rem;
+    }
+  }
+
   .left-content {
     width: 100%;
 
