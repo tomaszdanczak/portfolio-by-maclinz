f182d4d7f2e70a24a8fba87caffd07fa360297b6
Tomasz Dańczak <tomaszdanczak@gmail.com>
2021-07-01 17:42:05 +0200

    feat: add media queries to the PortfolioItems component

--- src/Components/PortfolioItems.jsx
+++ src/Components/PortfolioItems.jsx
@@ -40,6 +40,14 @@ const PortfolioItemsStyled = styled.div`
   grid-template-columns: repeat(3, 1fr);
   grid-gap: 2rem;
 
+  @media screen and (max-width: 920px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  @media screen and (max-width: 670px) {
+    grid-template-columns: repeat(1, 1fr);
+  }
+
   .grid-item {
     .portfolio-content {
       display: block;
